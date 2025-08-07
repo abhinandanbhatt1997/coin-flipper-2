15e4398a15d713d3860c29b4bbbbdb453deafec2
abhinandanbhatt1997 <160484334+abhinandanbhatt1997@users.noreply.github.com>
2025-08-07 14:32:54 +0530

    Updated supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-VITE_SUPABASE_URL=kflzyfonuzdvmnztdojt.supabase.co;
-VITE_SUPABASE_ANON_KEY='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtmbHp5Zm9udXpkdm1uenRkb2p0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM3MTU2NDgsImV4cCI6MjA2OTI5MTY0OH0.wmqHFQzVMG2uXxEyI2opm2D4jhrrZRflSAaYkBg1EEw';
+const supabaseUrl = 'http://127.0.0.1:54321';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
